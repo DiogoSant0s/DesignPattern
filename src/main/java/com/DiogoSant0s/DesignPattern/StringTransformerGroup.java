076d16ab1f6fb697ad95fa29c2a9d1af7ba950b6
DiogoSant0s <up202009291@up.pt>
2022-11-09 00:40:53 +0000

    COMPOSITE PATTERN

---
+++ src/main/java/com/DiogoSant0s/DesignPattern/StringTransformerGroup.java
@@ -0,0 +1,22 @@
+package com.DiogoSant0s.DesignPattern;
+
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer{
+    private final List<StringTransformer> transformers;
+    public StringTransformerGroup(List<StringTransformer> transformers) {
+        this.transformers = transformers;
+    }
+    @Override
+    public void execute(StringDrink drink) {
+        for (StringTransformer stringTransformer : transformers) {
+            stringTransformer.execute(drink);
+        }
+    }
+    @Override
+    public void undo(StringDrink drink) {
+        for (StringTransformer stringTransformer : transformers) {
+            stringTransformer.undo(drink);
+        }
+    }
+}
